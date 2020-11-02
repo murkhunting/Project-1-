14b5c05f85cb3519dfc5eb0dfa7d08dceb84d10a
Murkhunting <marcmp.arq@gmail.com>
2020-11-02 16:28:41 +0100

    add last html pages and prove apis

--- scripts/Api.js
+++ scripts/Api.js
@@ -1,8 +1,9 @@
 const apiKey = "orinHFtKbvLxKtP07t6KdHVA6AcakglvUD5VNaJH";
+
 async function getFirstData() {
   try {
     const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      `https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&format=ipac&where=pl_kepflag=1`
     );
     console.log("response", response);
     const dataAPI = await response.json();
